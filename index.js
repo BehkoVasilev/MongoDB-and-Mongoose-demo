61d9896f02e74f8e783d3599fe7f769b1568fb34
BehkoVasilev <vencislavvsilev@gmail.com>
2023-01-24 23:47:42 +0200

    add model schema

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const mongoose = require('mongoose');
 
-async function main(){
+async function main() {
     mongoose.set('strictQuery', false);
     mongoose.connect("mongodb://127.0.0.1:27017/catShelter");
-    
+
     console.log("Database connected!");
+
+    const catSchema = new mongoose.Schema({
+        age: Number,
+        name: String,
+        breed: String
+    });
+
 };
 
 main()
