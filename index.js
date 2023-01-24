3b017ffd89745d052498a1d1671281b135d70c64
BehkoVasilev <vencislavvsilev@gmail.com>
2023-01-25 00:19:55 +0200

    add model and create cat

--- index.js
+++ index.js
@@ -5,13 +5,34 @@ async function main() {
     mongoose.connect("mongodb://127.0.0.1:27017/catShelter");
 
     console.log("Database connected!");
-
-    const catSchema = new mongoose.Schema({
-        age: Number,
-        name: String,
-        breed: String
-    });
+    // await Cat.deleteMany({name: "Nav"});
+    await createCat("Nav", 8, "persian")
+    await readCats();
 
 };
+
+const catSchema = new mongoose.Schema({
+
+    name: String,
+    age: Number,
+    breed: String
+});
+
+const Cat = mongoose.model('Cat', catSchema);
+
+async function readCats() {
+    const cats = await Cat.find();
+    console.log(cats);
+};
+
+async function createCat(name, age, breed) {
+    const cat = new Cat({
+        name,
+        age,
+        breed
+    });
+
+    await cat.save();
+}
 
 main()
