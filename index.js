5322a598a12db0fc81d23eb3c7f15b6295229918
BehkoVasilev <vencislavvsilev@gmail.com>
2023-01-25 00:39:38 +0200

    add schema method

--- index.js
+++ index.js
@@ -1,38 +1,51 @@
 const mongoose = require('mongoose');
 
+const catSchema = new mongoose.Schema({
+    name: String,
+    age: Number,
+    breed: String
+});
+
+catSchema.methods.sayMeow = function () {
+    console.log(`${this.name} is ${this.age} years old - Meow...`);
+};
+
+const Cat = mongoose.model('Cat', catSchema);
+
 async function main() {
     mongoose.set('strictQuery', false);
     mongoose.connect("mongodb://127.0.0.1:27017/catShelter");
 
     console.log("Database connected!");
     // await Cat.deleteMany({name: "Nav"});
-    await createCat("Nav", 8, "persian")
-    await readCats();
+    // await createCat("Misho", 4, "streetcat")
+
+    const cats = await readCats();
+    cats.forEach(cat => cat.sayMeow());
 
 };
-
-const catSchema = new mongoose.Schema({
-
-    name: String,
-    age: Number,
-    breed: String
-});
-
-const Cat = mongoose.model('Cat', catSchema);
 
 async function readCats() {
     const cats = await Cat.find();
+
     console.log(cats);
+
+    return cats
 };
 
 async function createCat(name, age, breed) {
-    const cat = new Cat({
+    await Cat.create({
         name,
         age,
-        breed
+        breed,
     });
 
-    await cat.save();
+    // const cat = new Cat({
+    //     name,
+    //     age,
+    //     breed
+    // });
+    // await cat.save();
 }
 
 main()
