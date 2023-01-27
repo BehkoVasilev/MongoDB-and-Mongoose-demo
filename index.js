2d3716b978727417dfc898c03debb8b232699057
BehkoVasilev <vencislavvsilev@gmail.com>
2023-01-27 20:05:49 +0200

    add validations

--- index.js
+++ index.js
@@ -1,18 +1,38 @@
 const mongoose = require('mongoose');
 
 const catSchema = new mongoose.Schema({
-    name: String,
-    age: Number,
-    breed: String
+    name: {
+        type: String,
+        required: [true, 'Must contain a name!']
+    },
+    age: {
+        type: Number,
+        min: 0,
+        max: 22
+    },
+    breed: {
+        type: String,
+        enum: ['himalayan', 'korat', 'angora', 'persian', 'streetcat', 'abyssinian']
+    },
+    vaccineNumber: {
+        type: String,
+        requred: [true, 'You must enter a vaccine number!'],
+        validate: {
+            validator: function (v) {
+                return /VAC[0-9]{3}-[0-9]{4}/.test(v);
+            },
+            message: props => `${props.value} is not a valid vaccine number!`
+        },
+    }
 });
 
 // add method
 catSchema.methods.sayMeow = function () {
-    console.log(`${this.name} - Meow...`);
+    console.log(`Hello my name is ${this.name} - Meow...`);
 };
 
 // Virtual property
-catSchema.virtual('info').get(function() {
+catSchema.virtual('info').get(function () {
     console.log(`${this.name} - ${this.age} age, ${this.breed}.`);
 });
 
@@ -25,13 +45,13 @@ async function main() {
     mongoose.connect("mongodb://127.0.0.1:27017/catShelter");
 
     console.log("Database connected!");
-    // await Cat.deleteMany({name: "Nav"});
-    // await createCat("Misho", 4, "streetcat")
+    // await Cat.deleteMany({name: "Misho"});
+    await createCat("Teddy", 16, "korat", "VAC323-3213")
 
     const cats = await readCats();
     cats.forEach(cat => {
         cat.sayMeow(),
-        cat.info 
+            cat.info
     });
 
 };
@@ -44,11 +64,12 @@ async function readCats() {
     return cats
 };
 
-async function createCat(name, age, breed) {
+async function createCat(name, age, breed, vaccineNumber) {
     await Cat.create({
         name,
         age,
         breed,
+        vaccineNumber
     });
 
     // const cat = new Cat({
